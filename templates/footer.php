1c2de1421937cbb006a766c1bbf21b6e8e4171b4
Drakhiro <vltjeje@gmail.com>
2025-05-19 13:56:18 +0200

    Comment git api

--- templates/footer.php
+++ templates/footer.php
@@ -13,21 +13,21 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 <?php
 //TODO:  Si l'utilisateur est connecte, on affiche un lien de deconnexion 
-$curl = curl_init();
+// $curl = curl_init();
 
-curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.football-data.org/v4/competitions/FL1/matches", // FL1 = Ligue 1
-    // CURLOPT_URL => "https://api.football-data.org/v4/competitions/CL/standings", // FL1 = Ligue 1
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => [
-        "X-Auth-Token: 96532e3c36b34947b91e86d8a334d869"
-    ],
-]);
+// curl_setopt_array($curl, [
+//     CURLOPT_URL => "https://api.football-data.org/v4/competitions/FL1/matches", // FL1 = Ligue 1
+//     // CURLOPT_URL => "https://api.football-data.org/v4/competitions/CL/standings", // FL1 = Ligue 1
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_HTTPHEADER => [
+//         "X-Auth-Token: 96532e3c36b34947b91e86d8a334d869"
+//     ],
+// ]);
 
-$response = curl_exec($curl);
-curl_close($curl);
+// $response = curl_exec($curl);
+// curl_close($curl);
 
-echo $response;
+// echo $response;
 tprint($_SESSION);
 if (valider("connected")) {
 	echo "<a href=\"controleur.php?action=Logout\">Se Déconnecter</a>";

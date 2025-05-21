81627f0d1b3ac3f772ab9fb7f17d0b67ed1b9d90
Drakhiro <vltjeje@gmail.com>
2025-05-21 21:58:40 +0200

    Suppression fonctions inutiles, ajout creation d'un utilisateur

--- libs/modele.php
+++ libs/modele.php
@@ -5,48 +5,33 @@ Dans ce fichier, on définit diverses fonctions permettant de récupérer des do
 */
 
 
-/********* PARTIE 1 : prise en main de la base de données *********/
-
 
 // inclure ici la librairie faciliant les requêtes SQL
+include_once("maLibSQL.pdo.php");
 
 
-function listerUtilisateurs($classe = "both")
-{
-	// Cette fonction liste les utilisateurs de la base de données 
-	// et renvoie un tableau d'enregistrements. 
-	// Chaque enregistrement est un tableau associatif contenant les champs 
-	// id,pseudo,blacklist,connecte,couleur
-
-	// Lorsque la variable $classe vaut "both", elle renvoie tous les utilisateurs
-	// Lorsqu'elle vaut "bl", elle ne renvoie que les utilisateurs blacklistés
-	// Lorsqu'elle vaut "nbl", elle ne renvoie que les utilisateurs non blacklistés
-
-}
-
-
-function interdireUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "blacklist" à vrai pour l'utilisateur concerné 
-}
-
-function autoriserUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "blacklist" à faux pour l'utilisateur concerné 
-}
-
-function verifUserBdd($login,$password)
+function verifUserBdd($identifiant,$password)
 {
 	// Vérifie l'identité d'un utilisateur 
 	// dont les identifiants sont passes en paramètre
 	// renvoie faux si user inconnu
 	// renvoie l'id de l'utilisateur si succès
 
-	$SQL="SELECT `id` FROM users WHERE `username`='$login' AND `password`='$password'";
+	$SQL="SELECT `id` FROM users WHERE (`username`='$identifiant' OR `mail`='$identifiant') AND `password`='$password'";
 
 	return SQLGetChamp($SQL);
 }
 
+function existInBdd($value, $attribut) {
+	$SQL="SELECT `id` FROM `users` WHERE `$attribut`='$value'; ";
+	// echo $SQL; die();
+	return SQLGetChamp($SQL);
+}
+
+function createUser($username, $mail, $mdp) {
+	$SQL="INSERT INTO `users` (`mail`, `username`, `password`) VALUES ('$mail', '$username', '$mdp');";
+	return SQLInsert($SQL);
+}
 
 function isAdmin($idUser)
 {
@@ -55,115 +40,6 @@ function isAdmin($idUser)
 	return SQLGetChamp($SQL); 
 }
 
-/********* PARTIE 2 *********/
-
-function mkUser($pseudo, $passe,$admin=false,$couleur="black")
-{
-	// Cette fonction crée un nouvel utilisateur et renvoie l'identifiant de l'utilisateur créé
-}
-
-function connecterUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "connecte" à vrai pour l'utilisateur concerné 
-}
-
-function deconnecterUtilisateur($idUser)
-{
-	// cette fonction affecte le booléen "connecte" à faux pour l'utilisateur concerné 
-}
-
-function changerCouleur($idUser,$couleur="black")
-{
-	// cette fonction modifie la valeur du champ 'couleur' de l'utilisateur concerné
-}
-
-function changerPasse($idUser,$passe)
-{
-	// cette fonction modifie le mot de passe d'un utilisateur
-}
-
-function changerPseudo($idUser,$pseudo)
-{
-	// cette fonction modifie le pseudo d'un utilisateur
-}
-
-function promouvoirAdmin($idUser)
-{
-	// cette fonction fait de l'utilisateur un administrateur
-}
-
-function retrograderUser($idUser)
-{
-	// cette fonction fait de l'utilisateur un simple mortel
-}
-
-
-/********* PARTIE 3 *********/
-
-function listerUtilisateursConnectes()
-{
-	// Liste les utilisteurs connectes
-}
-
-function listerConversations($mode="tout")
-{
-	// Liste toutes les conversations ($mode="tout")
-	// OU uniquement celles actives  ($mode="actives"), ou inactives  ($mode="inactives")
-}
-
-function archiverConversation($idConversation)
-{
-	// rend une conversation inactive
-}
-
-function creerConversation($theme)
-{
-	// crée une nouvelle conversation et renvoie son identifiant
-}
-
-function reactiverConversation($idConversation)
-{	
-	// rend une conversation active
-
-}
-
-function supprimerConversation($idConv)
-{
-	// supprime une conversation et ses messages
-
-	// NB : on aurait pu aussi demander à mysql de supprimer automatiquement
-	// les messages lorsqu'une conversation est supprimée, 
-	// en déclarant idConversation comme clé étrangère vers le champ id de la table 
-	// des conversations et en définissant un trigger
-}
-
-
-function enregistrerMessage($idConversation, $idAuteur, $contenu)
-{
-	// Enregistre un message dans la base en encodant les caractères spéciaux HTML : <, > et & pour interdire les messages HTML
-}
-
-function listerMessages($idConv,$format="asso")
-{
-	// Liste les messages de cette conversation, au format JSON ou tableau associatif
-	// Champs à extraire : contenu, auteur, couleur 
-	// en ne renvoyant pas les utilisateurs blacklistés
-	
-}
-
-function listerMessagesFromIndex($idConv,$index)
-{
-	// Liste les messages de cette conversation, 
-	// dont l'id est superieur à l'identifiant passé
-	// Champs à extraire : contenu, auteur, couleur 
-	// en ne renvoyant pas les utilisateurs blacklistés
-
-}
-
-function getConversation($idConv)
-{	
-	// Récupère les données de la conversation (theme, active)
-}
 
 
 
